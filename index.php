ae08d8ba66128677388d14d505dfb4470a866d70
Thijs van der Woude <thijsvanderwoude99@gmail.com>
2021-08-31 00:05:41 +0200

    Start routing element

--- index.php
+++ index.php
@@ -31,14 +31,11 @@ $url = $request->getUrl();
 // write majorak tool that generates a switch and includes it here
 
 foreach($routes as $route) {
-    d($route);
     if($route = "home") {
-        
         $response->setContent("Hello from Majorak!")->send();
-        //exit(0);
+        exit(0);
     }
 }
-
 // If we make it to this point, we don't know what has been requested. Thusly, 404.
 $response->setContent("Error 404, page not found!")->setStatus(404)->send();
 
