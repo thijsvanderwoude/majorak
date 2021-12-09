7a6e2f02df456bb8bc95ede76ab78dec48f9b03f
Thijs van der Woude <thijsvanderwoude99@gmail.com>
2021-12-09 21:05:50 +0100

    Finalize stock 404 design.

--- routes/404/index.php
+++ routes/404/index.php
@@ -1,5 +1,15 @@
 ?>
 
-yo
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>404</title>
+    </head>
+    <body>
+        <h1>Error 404</h1>
+        <hr>
+        <p>The page you were looking for does not exist.</p>
+    </body>
+</html>
 
 <?php
