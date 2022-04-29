8b393bd1c5d722ee9cca8bf3791f40184e6efaea
Thijs van der Woude <thijsvanderwoude99@gmail.com>
2022-04-29 20:54:24 +0200

    Change to get real url

--- majorak/Http/Request.php
+++ majorak/Http/Request.php
@@ -7,17 +7,9 @@
 namespace Majorak\Http;
 
 class Request {
-    protected $get, $post, $server;
-
-    function __construct() {
-        $this->get = $_GET;
-        $this->post = $_POST;
-        $this->server = $_SERVER;
-    }
-
     public function getUrl() {
-        if(array_key_exists("REQUEST_URI", $this->server)) {
-            return $this->server["REQUEST_URI"];
+        if(array_key_exists("PHP_SELF", $_SERVER)) {
+            return $_SERVER["PHP_SELF"];
         }
         return "";
     }
