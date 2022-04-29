228bb88d78eb50b5811d552a22eb6b8ea1f7c9a0
Thijs van der Woude <thijsvanderwoude99@gmail.com>
2022-04-29 20:35:37 +0200

    Add Component/Domain.php & Component/Responder.php

--- majorak/require.php
+++ majorak/require.php
@@ -2,3 +2,5 @@
 require_once "Http/Request.php";
 require_once "Http/Response.php";
 require_once "Component/Action.php";
+require_once "Component/Domain.php";
+require_once "Component/Responder.php";
