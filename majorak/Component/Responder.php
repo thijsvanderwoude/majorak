3e9b9eb613657beb3cd7f88c1b072edbd1f0ea12
Thijs van der Woude <thijsvanderwoude99@gmail.com>
2022-05-08 16:43:41 +0200

    The Responder needs to return a Response object to the Router. Logic implemented

--- majorak/Component/Responder.php
+++ majorak/Component/Responder.php
@@ -8,7 +8,11 @@
 namespace Majorak\Component;
 
 class Responder {
-    
+    protected $response;
+
+    public function __construct(\Majorak\Http\Response $response) {
+        $this->response = $response;
+    }
 }
 
 ?>
