d2422b6104fe213759f11cb4c1b3b284de57278c
Thijs van der Woude <thijsvanderwoude99@gmail.com>
2022-05-08 19:18:53 +0200

    Update requires

--- majorak/Requires.php
+++ majorak/Requires.php
@@ -2,7 +2,7 @@
 
 include("Component/Action.php");
 include("Component/Domain.php");
-include("Component/DomainResponse.php");
+include("Component/DomainPayload.php");
 include("Component/Responder.php");
 include("Http/Response.php");
 include("Http/Request.php");
