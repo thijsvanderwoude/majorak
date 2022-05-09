d7d0cfaabdb16b3966693e51a81d6620a264cf65
Thijs van der Woude <thijsvanderwoude99@gmail.com>
2022-05-09 02:15:46 +0200

    Add template rendering to the init example

--- bin/init/src/routes/index/indexResponder.php
+++ bin/init/src/routes/index/indexResponder.php
@@ -3,24 +3,16 @@
 namespace App;
 
 use Majorak\Component\Responder;
+use Majorak\Templatery\Render;
 
 class AppResponder extends Responder {
     public function __invoke() {
-        $page = "
-        <!DOCTYPE html>
-        <html>
-            <head>
-                <title>Majorak!</title>
-            </head>
-            <body>
-                <h1>It works!</h1>
-                <p>If you're seeing this, Majorak is working correctly.</p>
-                <p>This app served by: " . $this->payload["server"] . " @ " . $this->payload["host"] . "<br><br>
-            </body>
-        </html>
-        ";
+        $payload = $this->payload;
 
-        return $this->response->setContent($page);
+        $render = new Render;
+        $render("indexTemplate", $payload);
+
+        return $this->response;
     }
 }
 
--- bin/init/src/templates/indexTemplate.php
+++ bin/init/src/templates/indexTemplate.php
@@ -1,2 +1,13 @@
-<a target="_blank" href="https://github.com/thijsvanderwoude/majorak">GitHub</a>
-<a target="_blank" href="https://github.com/thijsvanderwoude/majorak/blob/main/LICENSE">License</a>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Majorak!</title>
+    </head>
+    <body>
+        <h1>It works!</h1>
+        <p>If you're seeing this, Majorak is working correctly.</p>
+        <p>This app served by: <?= $payload["server"] ?> @ <?= $payload["host"] ?><br><br>
+        <a target="_blank" href="https://github.com/thijsvanderwoude/majorak">GitHub</a>
+        <a target="_blank" href="https://github.com/thijsvanderwoude/majorak/blob/main/LICENSE">License</a>
+    </body>
+</html>
