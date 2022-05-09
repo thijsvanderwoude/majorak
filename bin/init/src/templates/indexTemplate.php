05e557d8b49404781605986cec0368c31e9dd1ee
Thijs van der Woude <thijsvanderwoude99@gmail.com>
2022-05-09 02:19:58 +0200

    Fix spelling mistake in the index template

--- bin/init/src/templates/indexTemplate.php
+++ bin/init/src/templates/indexTemplate.php
@@ -6,7 +6,7 @@
     <body>
         <h1>It works!</h1>
         <p>If you're seeing this, Majorak is working correctly.</p>
-        <p>This app served by: <?= $payload["server"] ?> @ <?= $payload["host"] ?><br><br>
+        <p>This app is served by: <?= $payload["server"] ?> @ <?= $payload["host"] ?><br><br>
         <a target="_blank" href="https://github.com/thijsvanderwoude/majorak">GitHub</a>
         <a target="_blank" href="https://github.com/thijsvanderwoude/majorak/blob/main/LICENSE">License</a>
     </body>
