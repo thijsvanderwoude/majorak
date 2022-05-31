14cfd7148660561b6c5458c3e2e63b7022ccdd22
Thijs van der Woude <thijsvanderwoude99@gmail.com>
2022-05-31 21:56:33 +0200

    Modify example template and change DomainPayload in indexDomain to Payload

--- bin/init/src/routes/index/indexDomain.php
+++ bin/init/src/routes/index/indexDomain.php
@@ -3,11 +3,11 @@
 namespace App;
 
 use Majorak\Component\Domain;
-use Majorak\Component\DomainPayload;
+use Majorak\Component\Payload;
 
 class AppDomain extends Domain {
     public function __invoke() {
-        $payload = new DomainPayload;
+        $payload = new Payload;
         $payload->addItem(["server" => $this->request->server["SERVER_SOFTWARE"]]);
         $payload->addItem(["host" => $this->request->server["HTTP_HOST"]]);
         return $payload;
